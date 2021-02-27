19a03fea79776d87408d4e1fc2e3ad63c41153b3
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-02-27 12:25:35 -0500

    update nav bar

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const Nav = () => {
   const [scrolled, setScrolled] = React.useState(false);
-  let navbarClasses=["nav"];
+  let navbarClasses=["nav-container"];
 
   const handleScroll = () => {
     const offset = window.scrollY;
@@ -24,11 +24,17 @@ const Nav = () => {
 
   return (
     <header className={navbarClasses.join(" ")}>
-      <div className="nav-logo">
-        <h1>Double Sessions</h1>
+      <div className="nav">
+        <div className="nav-logo">
+          <h1>Double Sessions</h1>
+        </div>
+        <nav className="navigation">
+          <ul>
+            <li>Search</li>
+            <li>Favorites</li>
+          </ul>
+        </nav>
       </div>
-      <nav className="navigation">
-      </nav>
     </header>
   )
 }
